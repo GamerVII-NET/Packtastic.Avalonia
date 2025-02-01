f8f0f511803795ed96c7e8cdc5508fea6a2e8866
GamerVII-NET <terentev.a.a@recloud.tech>
2025-02-01 07:20:41 +0300

    Add build project display and loading logic to UI
    Introduce a `BuildPageViewModel` to load and organize build projects asynchronously using ReactiveUI and Avalonia's UI thread. Update the `BuildPage` view to reflect the build project list with data bindings, a progress bar, and error handling for empty or failed project loading cases.

--- Packtastic.Avalonia.Desktop/ViewModels/Base/PageViewModelBase.cs
+++ Packtastic.Avalonia.Desktop/ViewModels/Base/PageViewModelBase.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using ReactiveUI.Fody.Helpers;
 
 namespace Packtastic.Avalonia.Desktop.ViewModels.Base;
 
 public class PageViewModelBase : ViewModelBase
 {
+    [Reactive] public bool IsProcessing { get; set; }
+    
     protected Task ExecuteFromNewThread(Func<Task> func)
     {
         var tcs = new TaskCompletionSource<object?>();
--- Packtastic.Avalonia.Desktop/ViewModels/BuildPageViewModel.cs
+++ Packtastic.Avalonia.Desktop/ViewModels/BuildPageViewModel.cs
@@ -1,11 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Concurrency;
+using Avalonia.Threading;
+using DynamicData.Binding;
+using Microsoft.Build.Exceptions;
 using Packtastic.Avalonia.Desktop.ViewModels.Base;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 
 namespace Packtastic.Avalonia.Desktop.ViewModels;
 
 public class BuildPageViewModel : PageViewModelBase
 {
+    private readonly PackManager _packManager;
+    private IReadOnlyCollection<IBuildProject> _buildProjects;
+
+    [Reactive] public ObservableCollectionExtended<IBuildProject> BuildProjects { get; set; } = [];
+    [Reactive] public IBuildProject? SelectedBuildProject { get; set; }
+    [Reactive] public bool BuildProjectsIsEmpty { get; set; }
+    
     public BuildPageViewModel(PackManager packManager)
     {
-        
+        _packManager = packManager;
+        RxApp.MainThreadScheduler.Schedule(LoadData);
+    }
+    
+    private void LoadData()
+    {
+        IsProcessing = true;
+        ExecuteFromNewThread(async () =>
+        {
+            try
+            {
+                _buildProjects = await _packManager.Projects.GetBuildProjectsAsync();
+
+                await Dispatcher.UIThread.InvokeAsync(() =>
+                {
+                    BuildProjects = new ObservableCollectionExtended<IBuildProject>(_buildProjects.OrderBy(c => c.Name));
+                    BuildProjectsIsEmpty = BuildProjects.Count == 0;
+                });
+            }
+            catch (InvalidProjectFileException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                await Dispatcher.UIThread.InvokeAsync(() =>
+                {
+                    IsProcessing = false;
+                });
+            }
+        });
     }
 }
