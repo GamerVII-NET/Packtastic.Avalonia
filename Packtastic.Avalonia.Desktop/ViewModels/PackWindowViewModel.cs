3674a764fad48220f822e519cebfab3c487a8066
GamerVII-NET <terentev.a.a@recloud.tech>
2025-02-01 06:39:58 +0300

    Add project selection and display functionality
    Implemented support for displaying and selecting projects within a chosen solution. Updated the view model to load projects dynamically and propagate selection states. Enhanced the UI to include a project selection ListBox and adjusted data bindings accordingly.

--- Packtastic.Avalonia.Desktop/ViewModels/PackWindowViewModel.cs
+++ Packtastic.Avalonia.Desktop/ViewModels/PackWindowViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Reactive.Concurrency;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
 using Avalonia.Platform.Storage;
 using Avalonia.Threading;
 using DynamicData.Binding;
+using Microsoft.Build.Exceptions;
 using Packtastic.Avalonia.Desktop.ViewModels.Base;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
@@ -17,16 +20,27 @@ public class PackWindowViewModel : PageViewModelBase
 {
     private readonly PackManager _packManager;
     private IReadOnlyCollection<ISolution> _solutions;
+    private IReadOnlyCollection<IProject> _projects;
     [Reactive] public ObservableCollectionExtended<ISolution> Solutions { get; set; }
+    [Reactive] public ObservableCollectionExtended<IProject> SolutionProjects { get; set; }
+    [Reactive] public ISolution? SelectedSolution { get; set; }
+    [Reactive] public IProject? SelectedProject { get; set; }
     [Reactive] public bool SolutionsIsEmpty { get; set; }
+    [Reactive] public bool ProjectsIsEmpty { get; set; }
     
     public PackWindowViewModel(PackManager? packManager = null)
     {
         _packManager = packManager 
                        ?? Locator.Current.GetService<PackManager>() 
                        ?? throw new Exception("No pack manager found");
+        
+        
 
         RxApp.MainThreadScheduler.Schedule(LoadData);
+        
+        this.WhenAnyValue(vm => vm.SelectedSolution)
+            .Where(selectedSolution => selectedSolution != null)
+            .Subscribe(LoadProjectsForSelectedSolution!);
 
     }
 
@@ -35,13 +49,39 @@ public class PackWindowViewModel : PageViewModelBase
         ExecuteFromNewThread(async () =>
         {
             _solutions = await _packManager.Projects.GetSolutionsAsync();
-
+    
             await Dispatcher.UIThread.InvokeAsync(() =>
             {
-                Solutions = new ObservableCollectionExtended<ISolution>(_solutions);
+                Solutions = new ObservableCollectionExtended<ISolution>(_solutions.OrderBy(c => c.Name));
                 SolutionsIsEmpty = Solutions.Count == 0;
             });
         });
+    }
+    
+    private void LoadProjectsForSelectedSolution(ISolution selectedSolution)
+    {
+        ExecuteFromNewThread(async () =>
+        {
+            try
+            {
+                _projects = await selectedSolution.GetProjectsAsync();
+    
+                await Dispatcher.UIThread.InvokeAsync(() =>
+                {
+                    SolutionProjects = new ObservableCollectionExtended<IProject>(_projects.OrderBy(c => c.Name));
+                    ProjectsIsEmpty = Solutions.Count == 0;
+                });
+            }
+            catch (InvalidProjectFileException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        });
+        
     }
 
     public async Task AddSolutionPaths(params IEnumerable<string> paths)
